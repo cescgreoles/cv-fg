4be48c12af17b3296411ceb59d480f27988e2027
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2023-11-26 10:55:26 +0100

    Update of the favicon and modification of the home page. IconsGitLin component implementation.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,7 +4,7 @@ import Education from "../components/Education";
 import Experience from "../components/Experience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
-import IconsGitLin from "../components/IconsGitLin";
+import IconsGitLin from "../components/IconsGitLin.jsx";
 import "../Home/Home.jsx";
 
 const Home = () => {
