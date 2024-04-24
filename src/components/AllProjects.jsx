8b6ad414e9aaa690e4d9bd02a4f98e51ab84d9f4
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2024-04-24 14:55:10 +0200

    check and push allprojects and the design

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -1,49 +1,29 @@
-import React, { useState } from "react";
 import { CV } from "../utils/cv";
 import { Link } from "react-router-dom"; // Importa Link desde react-router-dom
 import "../styles/AllProjects.scss"; // Asegúrate de importar tu archivo de estilos SCSS
 
 const AllProjects = () => {
-  const [isPersonalOpen, setIsPersonalOpen] = useState(false);
-  const [isBootcampOpen, setIsBootcampOpen] = useState(false);
-
-  const togglePersonal = () => {
-    setIsPersonalOpen(!isPersonalOpen);
-  };
-
-  const toggleBootcamp = () => {
-    setIsBootcampOpen(!isBootcampOpen);
-  };
-
   return (
     <div className="all-projects-container">
       <div className="projects-section">
-        <button onClick={toggleBootcamp} className="project-button">
-          {isBootcampOpen ? "Bootcamp Projects" : "Bootcamp Projects"}
-        </button>
-        {isBootcampOpen && (
-          <div className="projects-list">
-            {CV.portfolio.map((info, index) => (
-              <Link to={info.url} className="project-item" key={index}>
-                {info.name}
-              </Link>
-            ))}
-          </div>
-        )}
+        <h3>My Bootcamp Projects</h3>
+        <div className="projects-list">
+          {CV.portfolio.map((info, index) => (
+            <Link to={info.url} className="project-item" key={index}>
+              <img src={info.img} alt={info.name}></img>
+            </Link>
+          ))}
+        </div>
       </div>
       <div className="projects-section">
-        <button onClick={togglePersonal} className="project-button">
-          {isPersonalOpen ? "Personal Projects" : "Personal Projects"}
-        </button>
-        {isPersonalOpen && (
-          <div className="projects-list">
-            {CV.portfolio1.map((info, index) => (
-              <Link to={info.url} className="project-item" key={index}>
-                {info.name1}
-              </Link>
-            ))}
-          </div>
-        )}
+        <h3>My Personal Projects</h3>
+        <div className="projects-list">
+          {CV.portfolio1.map((info, index) => (
+            <Link to={info.url} className="project-item" key={index}>
+              <img src={info.img} alt={info.name}></img>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
